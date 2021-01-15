99b88141498ac1caa39de8f180a83d281739ae95
Kritika Kumari <51400849+kritika122000@users.noreply.github.com>
2021-01-15 20:24:03 +0530

    Create 1646.Get_maximum_in_generated_array.cpp

---
+++ 1646.Get_maximum_in_generated_array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int getMaximumGenerated(int n)
+    {
+        if(n==0|| n==1)return n ;
+        vector <int> arr(n+1);
+        arr[0]=0;
+        arr[1]=1;
+        int ans =   1;
+        for (int i = 2; i <= n; i++) {
+            arr[i] = i % 2 == 0 ? arr[i/2] : arr[i / 2] + arr[i / 2 + 1];
+            ans = max(ans, arr[i]);
+        }
+            return ans;
+        }
+};
